061763c21b0e88107e7bf1288cefc4ff075d3760
Einar Ólafsson <363286+einarolafs@users.noreply.github.com>
2024-10-31 17:08:33 +0100

    Update extract-jira-tickets.rb

--- extract-jira-tickets.rb
+++ extract-jira-tickets.rb
@@ -1,8 +1,8 @@
 class ExtractJiraTickets < Formula
   desc "Extract Jira tickets from git history between two tags"
-  homepage "https://github.com/sosafe-einar-olafsson/extract-jira-tickets"
-  url "https://github.com/sosafe-einar-olafsson/extract-jira-tickets/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f05d82ea1795bd4b85a52c60b2162acebf1b1dc3384995f1cc6677eb47b4d16c"
+  homepage "https://github.com/einarolafs/extract-jira-tickets"
+  url "https://github.com/einarolafs/extract-jira-tickets/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "89607ce1cca460259095cf8007c0e63816f28f42939dcea14a854c41e90308a9"
   version "0.1.0"
 
   def install
